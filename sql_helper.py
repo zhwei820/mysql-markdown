d929a7c94eed479ecb9452b09c2bdda4d59f7d30
周伟 <wei.zhou@matrixport.com>
2022-11-29 16:08:55 +0800

    remove db util

--- sql_helper.py
+++ sql_helper.py
@@ -1,8 +1,6 @@
 import logging
 
 import pymysql
-from dbutils.persistent_db import PersistentDB
-from pymysql.constants import CLIENT
 
 _logger = logging.getLogger(__name__)
 
@@ -10,39 +8,19 @@ _logger = logging.getLogger(__name__)
 class SQLHelper(object):
     """
     user dbutils create connection pool
-    see reference https://webwareforpython.github.io/DBUtils/main.html
     """
 
     def __init__(self, **kwargs):
-        self.__connection_pool = PersistentDB(
-            creator=pymysql,  # 使用链接数据库的模块
-            maxusage=None,  # 一个链接最多被重复使用的次数，None表示无限制
-            host=kwargs["host"],
-            port=kwargs["port"],
-            user=kwargs["user"],
-            password=kwargs.get("password"),
-            database=kwargs["database"],
-            charset=kwargs.get("charset", "utf8mb4"),
-            client_flag=CLIENT.MULTI_STATEMENTS,
-        )
+        self.db = pymysql.connect(**kwargs)
 
     def connection(self, cursor):
-        conn = self.__connection_pool.connection()
-        cursor = conn.cursor(cursor=cursor)
-        return conn, cursor
-
-    @staticmethod
-    def close(conn, cursor):
-        conn.commit()
-        cursor.close()
-        conn.close()
+        return self.db, self.db.cursor(cursor)
 
     def fetch_one(self, sql, args=None, cursor=pymysql.cursors.DictCursor):
         _logger.debug("exec sql %s and params %s", sql, args)
         conn, cursor = self.connection(cursor)
         cursor.execute(sql, args)
         obj = cursor.fetchone()
-        self.close(conn, cursor)
         return obj
 
     def fetch_all(self, sql, args=None, cursor=pymysql.cursors.DictCursor):
@@ -50,7 +28,6 @@ class SQLHelper(object):
         conn, cursor = self.connection(cursor)
         cursor.execute(sql, args)
         obj = cursor.fetchall()
-        self.close(conn, cursor)
         return obj
 
     def update(self, sql, args=None, cursor=pymysql.cursors.DictCursor):
@@ -59,8 +36,8 @@ class SQLHelper(object):
     def execute(self, sql, args=None, cursor=pymysql.cursors.DictCursor):
         conn, cursor = self.connection(cursor)
         result = cursor.execute(sql, args)
-        self.close(conn, cursor)
         return result
+
 
 
 if __name__ == "__main__":
